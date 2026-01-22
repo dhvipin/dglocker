f35d23ea104fb549304be8dd7b5f224f21b1fea3
Vipin Dhingra <141640649+dhvipin@users.noreply.github.com>
2026-01-22 19:07:39 +0530

    Delete backend directory

--- backend/upload_handler.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_FILES['document'])) {
-  $name = time().'_'.$_FILES['document']['name'];
-  move_uploaded_file($_FILES['document']['tmp_name'], "../uploads/".$name);
-  header("Location: ../dashboard.php");
-}
